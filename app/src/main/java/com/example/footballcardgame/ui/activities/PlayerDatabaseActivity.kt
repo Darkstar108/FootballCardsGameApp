32dee65069eaab37b525f24c1e5394a64c792764
Pranav Rai <pranav.rai@publicissapient.com>
2022-07-22 14:43:16 +0530

    Created project

---
+++ app/src/main/java/com/example/footballcardgame/ui/activities/PlayerDatabaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.footballcardgame.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerDatabaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_database)
+    }
+}
---
+++ app/src/main/java/com/example/footballcardgame/ui/adapters/PlayerDatabaseAdapter.kt
@@ -0,0 +1,2 @@
+package com.example.footballcardgame.ui.adapters
+
